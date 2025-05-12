fc699c02b06acdff040a20b662fa6f4e72dcfa8e
halfapear <1783148122@qq.com>
2025-05-13 00:12:57 +0800

    5.13(1)

--- src/java/com/shapeville/main/MainApp.java
+++ src/java/com/shapeville/main/MainApp.java
@@ -1,5 +1,17 @@
-package src.main.shapeville.main;
+package src.main.java.com.shapeville.main;
 
+import javax.swing.*;
+
+/**
+ * Main application entry point. Initializes and displays the main frame.
+ */
 public class MainApp {
-    
+
+    public static void main(String[] args) {
+        // Ensure UI operations are on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    }
 }
---
+++ src/java/com/shapeville/ui/NavigationBar.java
@@ -0,0 +1,5 @@
+package src.java.com.shapeville.ui;
+
+public class NavigationBar {
+    
+}
