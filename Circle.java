793b07890472dbbba13dbe3e020587a0472c73a5
ljjstone <157020819+ljjstone@users.noreply.github.com>
2025-05-18 15:11:58 +0800

    Delete Circle.java

--- Circle.java
+++
@@ -1,6 +0,0 @@
-package shapeville;
-
-public record Circle(int radius) {
-    public double area()          { return Math.PI * radius * radius; }
-    public double circumference() { return 2 * Math.PI * radius; }
-}
