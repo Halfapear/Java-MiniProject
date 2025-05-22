1dbc5dc7c6247c1fc079ca2bf3af934976eb5711
JohanHuang2005 <3138766352@qq.com>
2025-05-22 22:19:45 +0800

    add level in home panel

--- src/main/java/com/shapeville/ui/panel_templates/HomeScreenPanel.java
+++ src/main/java/com/shapeville/ui/panel_templates/HomeScreenPanel.java
@@ -45,13 +45,13 @@ public class HomeScreenPanel extends JPanel {
         tasksPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         tasksPanel.setBackground(new Color(50, 50, 50));
         
-        tasksPanel.add(createTaskButton("2D Shape Identification", Constants.TASK_ID_SHAPE_ID_2D));
-        tasksPanel.add(createTaskButton("3D Shape Identification", Constants.TASK_ID_SHAPE_ID_3D));
-        tasksPanel.add(createTaskButton("Angle Recognition", Constants.TASK_ID_ANGLE_TYPE));
-        tasksPanel.add(createTaskButton("Area Calculation", Constants.TASK_ID_AREA_CALC));
-        tasksPanel.add(createTaskButton("Circle Calculation", Constants.TASK_ID_CIRCLE_CALC));
-        tasksPanel.add(createTaskButton("Compound Area", Constants.TASK_ID_COMPOUND_AREA_CALC));
-        tasksPanel.add(createTaskButton("Sector Circle Calculation", Constants.TASK_ID_SECTOR_CIRCLE_CALC));
+        tasksPanel.add(createTaskButton("2D Shape Identification (KS1-TASK1)", Constants.TASK_ID_SHAPE_ID_2D));
+        tasksPanel.add(createTaskButton("3D Shape Identification (KS1-TASK1)", Constants.TASK_ID_SHAPE_ID_3D));
+        tasksPanel.add(createTaskButton("Angle Recognition (KS1-TASK2)", Constants.TASK_ID_ANGLE_TYPE));
+        tasksPanel.add(createTaskButton("Area Calculation (KS2-TASK3)", Constants.TASK_ID_AREA_CALC));
+        tasksPanel.add(createTaskButton("Circle Calculation (KS2-TASK4)", Constants.TASK_ID_CIRCLE_CALC));
+        tasksPanel.add(createTaskButton("Compound Area (BONUS1)", Constants.TASK_ID_COMPOUND_AREA_CALC));
+        tasksPanel.add(createTaskButton("Sector Circle Calculation (BONUS2)", Constants.TASK_ID_SECTOR_CIRCLE_CALC));
 
         add(tasksPanel, BorderLayout.CENTER);
     }
